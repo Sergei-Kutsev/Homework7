11f562fbae18a4084515f65347f31594a371a39c
Сергей Куцев <sergeikutsev@yandex.ru>
2021-10-15 22:20:08 +0300

    Добавьте файлы проекта.

---
+++ Homework7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше (создать метод для вычисления  площади  треугольника по длинам его сторон).  
+			//Для решения задачи можно использовать формулу Герона, где x, y, z – стороны треугольника, p – полупериметр.
+			Console.WriteLine("Введите длины сторон первого треугольника");
+			double x = Convert.ToDouble(Console.ReadLine());
+			double y = Convert.ToDouble(Console.ReadLine());
+			double z = Convert.ToDouble(Console.ReadLine());
+			double S1 = MetodArea(x, y, z);
+			Console.WriteLine("Площадь треугольника равна {0:f2}", S1);
+
+			Console.WriteLine("Введите длины сторон второго треугольника");
+			x = Convert.ToDouble(Console.ReadLine());
+			y = Convert.ToDouble(Console.ReadLine());
+			z = Convert.ToDouble(Console.ReadLine());
+			double S2 = MetodArea(x, y, z);
+			Console.WriteLine("Площадь треугольника равна {0:f2}", S2);
+
+			if (S1 > S2)
+				Console.WriteLine("Площадь первого треугольника больше: {0:f2} > {1:f2}", S1, S2);
+			if (S2 > S1)
+				Console.WriteLine("Площадь второго треугольника больше: {1:f2} > {0:f2}", S1, S2);
+			if (S1 == S2)
+				Console.WriteLine("Площади треугольников равны: {0:f2} = {1:f2}", S1, S2);
+			Console.ReadKey();
+		}
+
+		static double MetodArea(double x, double y, double z)
+		{
+			double P = x + y + z;
+			double p = P / 2;
+			double S = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+			return S;
+		}
+
+
+
+	}
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+			Console.WriteLine("Введите длину ребра куба");
+			double n = Convert.ToDouble(Console.ReadLine());
+			MetodAreaVolume(n);
+		}
+		static void MetodAreaVolume(double n)
+		{
+			double S = Math.Pow(n, 2) * 6;
+			double V = Math.Pow(n, 3);
+			Console.WriteLine("Площадь поверхности куба равна {0:f2}", S);
+			Console.WriteLine("Объем куба равен {0:f2}", V);
+			Console.ReadKey();
+		}
+	}
+}
